67559ae322b76b4e090d1b6935e0ed00e2b06beb
Kirill <kirunya1993@gmail.com>
2023-11-28 20:49:11 +0100

    Added conversion service to Rate service

--- expense-manager/src/main/java/com/testtask/expensemanager/services/RateService.java
+++ expense-manager/src/main/java/com/testtask/expensemanager/services/RateService.java
@@ -4,6 +4,7 @@ import com.testtask.expensemanager.core.dtos.RateCreateDto;
 import com.testtask.expensemanager.dao.api.IRateDao;
 import com.testtask.expensemanager.dao.entyties.Rate;
 import com.testtask.expensemanager.services.api.IRateService;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,8 +14,12 @@ public class RateService implements IRateService {
 
     private final IRateDao rateDao;
 
-    public RateService(IRateDao rateDao) {
+    private final ConversionService conversionService;
+
+    public RateService(IRateDao rateDao,
+                       ConversionService conversionService) {
         this.rateDao = rateDao;
+        this.conversionService = conversionService;
     }
 
     @Override
