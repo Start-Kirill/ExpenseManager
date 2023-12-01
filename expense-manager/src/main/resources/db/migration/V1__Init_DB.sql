5b7cff502aaff92adce7f47a5311a6081fe4ecc7
Kirill <kirunya1993@gmail.com>
2023-12-01 16:06:54 +0100

    Added migration scripts

---
+++ expense-manager/src/main/resources/db/migration/V1__Init_DB.sql
@@ -0,0 +1,79 @@
+CREATE TABLE app.currencies
+(
+    uuid uuid,
+    name text NOT NULL,
+    PRIMARY KEY (uuid),
+    UNIQUE (name)
+);
+
+ALTER TABLE IF EXISTS app.currencies
+    OWNER to expense_manager;
+
+CREATE TABLE app.currency_rates
+(
+    uuid uuid,
+    first_currency_uuid uuid NOT NULL,
+    second_currency_uuid uuid NOT NULL,
+    value numeric NOT NULL,
+    date timestamp without time zone NOT NULL,
+    PRIMARY KEY (uuid),
+    FOREIGN KEY (first_currency_uuid)
+        REFERENCES app.currencies (uuid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    FOREIGN KEY (second_currency_uuid)
+        REFERENCES app.currencies (uuid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
+
+ALTER TABLE IF EXISTS app.currency_rates
+    OWNER to expense_manager;
+
+CREATE TABLE app.limits
+(
+    uuid uuid,
+    expense_category text NOT NULL,
+    limit_sum numeric NOT NULL,
+    currency_uuid uuid NOT NULL,
+    datetime_create timestamp without time zone NOT NULL,
+    PRIMARY KEY (uuid),
+    FOREIGN KEY (currency_uuid)
+        REFERENCES app.currencies (uuid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
+
+ALTER TABLE IF EXISTS app.limits
+    OWNER to expense_manager;
+
+CREATE TABLE app.transactions
+(
+    uuid uuid,
+    currency_uuid uuid NOT NULL,
+    expense_category text NOT NULL,
+    account_from text NOT NULL,
+    account_to text NOT NULL,
+    trans_sum numeric NOT NULL,
+    trans_sum_in_usd numeric NOT NULL,
+    limit_uuid uuid NOT NULL,
+    exceeded boolean NOT NULL,
+    datetime timestamp without time zone NOT NULL,
+    PRIMARY KEY (uuid),
+    FOREIGN KEY (currency_uuid)
+        REFERENCES app.currencies (uuid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    FOREIGN KEY (limit_uuid)
+        REFERENCES app.limits (uuid) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
+
+ALTER TABLE IF EXISTS app.transactions
+    OWNER to expense_manager;
---
+++ expense-manager/src/main/resources/db/migration/V2__add_currencies.sql
@@ -0,0 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+INSERT INTO app.currencies(
+	uuid, name)
+	VALUES (uuid_generate_v4(), 'USD'),
+	       (uuid_generate_v4(), 'RUB'),
+	       (uuid_generate_v4(), 'KZT');
